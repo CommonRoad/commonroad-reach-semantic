27b4954d4069f6e3758aa2de08bd4f101190f1d9
Florian Lercher <florian.lercher@tum.de>
2023-04-19 16:54:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import commonroad_reach.utility.logger as util_logger
+from commonroad_reach.data_structure.configuration_builder import ConfigurationBuilder
+from commonroad_reach.data_structure.reach.reach_interface import ReachableSetInterface
+from commonroad_reach.utility import visualization as util_visual
+
+
+def main():
+    # ==== specify scenario
+    # name_scenario = "DEU_Test-1_1_T-1"
+    # name_scenario = "ZAM_Over-1_1"
+    name_scenario = "_ARG_Carcarana-1_1_T-1"
+    # name_scenario = "USA_US101-6_1_T-1"
+    # name_scenario = "ZAM_Intersection-1_1_T-1"
+
+    # ==== build configuration
+    config = ConfigurationBuilder.build_configuration(name_scenario, path_root="/home/lercher/tum/commonroad/commonroad-reach-semantic-addon")
+    config.update()
+    util_logger.initialize_logger(config)
+    config.print_configuration_summary()
+
+    # ==== compute reachable sets using reachability interface
+    reach_interface = ReachableSetInterface(config)
+    reach_interface.compute_reachable_sets()
+
+    # ==== plot computation results
+    util_visual.plot_collision_checker(reach_interface)
+    util_visual.plot_scenario_with_reachable_sets(reach_interface)
+
+
+if __name__ == "__main__":
+    main()
