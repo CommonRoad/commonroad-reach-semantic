649794bbf8ad560e7d9977394c33776f7317dfde
Florian Lercher <florian.lercher@tum.de>
2023-05-12 17:10:26 +0200

    Fix dictionary extraction in C++ labeling reach set

--- commonroad_reach_semantic/data_structure/reach/semantic_labeling_reach_set_cpp.py
+++ commonroad_reach_semantic/data_structure/reach/semantic_labeling_reach_set_cpp.py
@@ -40,9 +40,9 @@ class CppSemanticLabelingReachableSet(SemanticReachableSet):
             self._reach.compute(step, step)
             self._list_steps_computed.append(step)
 
-        self.dict_step_to_drivable_area = self._reach.drivable_area
-        self.dict_step_to_reachable_set = self._reach.reachable_set
-        self.dict_step_to_propagated_set = self._reach.propagated_set
+        self.dict_step_to_drivable_area = self._reach.drivable_area()
+        self.dict_step_to_reachable_set = self._reach.reachable_set()
+        self.dict_step_to_propagated_set = self._reach.propagated_set()
 
         if self.config.reachable_set.prune_nodes_not_reaching_final_step:
             self.prune_nodes_not_reaching_final_step()
