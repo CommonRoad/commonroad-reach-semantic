59506e152a6e67359c0cbc24d97bc2ee39634113
Florian Lercher <florian.lercher@tum.de>
2023-05-16 10:53:53 +0200

    Add Python bindings for C++ implementation of splitting OTF

--- python_binding/pybind_reachset.cpp
+++ python_binding/pybind_reachset.cpp
@@ -9,25 +9,35 @@ void export_reach(py::module &m) {
 }
 
 void export_reachable_set_interface(py::module &m) {
-    py::class_<SemanticLabelingReachableSet, shared_ptr<SemanticLabelingReachableSet>>(m, "SemanticLabelingReachableSet")
+    py::class_<SemanticReachableSet, shared_ptr<SemanticReachableSet>>(m, "SemanticReachableSet")
+            .def_readonly("step_start", &SemanticReachableSet::step_start)
+            .def_readonly("step_end", &SemanticReachableSet::step_end)
+            .def_readonly("config", &SemanticReachableSet::config)
+            .def_readonly("labeler", &SemanticReachableSet::labeler)
+            .def("compute", &SemanticReachableSet::compute, py::arg("step_start") = 1, py::arg("step_end") = 0)
+            .def("drivable_area_at_step", &SemanticReachableSet::drivable_area_at_step, py::arg("step"))
+            .def("reachable_set_at_step", &SemanticReachableSet::reachable_set_at_step, py::arg("step"))
+            .def("drivable_area", &SemanticReachableSet::drivable_area)
+            .def("reachable_set", &SemanticReachableSet::reachable_set)
+            .def("propagated_set", &SemanticReachableSet::propagated_set);
+
+    py::class_<SemanticLabelingReachableSet, shared_ptr<SemanticLabelingReachableSet>, SemanticReachableSet>(m, "SemanticLabelingReachableSet")
             .def(py::init<SemanticConfigurationPtr const &, CollisionCheckerPtr const &,
                          SemanticModelPtr const &, TrafficRuleInterfacePtr const &>(),
                  py::arg("configuration"),
                  py::arg("collision_checker"),
                  py::arg("semantic_model"),
                  py::arg("traffic_rule_interface"))
-            .def_readonly("step_start", &SemanticLabelingReachableSet::step_start)
-            .def_readonly("step_end", &SemanticLabelingReachableSet::step_end)
             .def_readonly("map_step_to_propositions_to_drivable_area",
-                          &SemanticLabelingReachableSet::map_step_to_propositions_to_drivable_area)
-            .def_readonly("config", &SemanticLabelingReachableSet::config)
-            .def_readonly("labeler", &SemanticLabelingReachableSet::labeler)
-            .def("compute", &SemanticLabelingReachableSet::compute, py::arg("step_start") = 1, py::arg("step_end") = 0)
-            .def("drivable_area_at_step", &SemanticLabelingReachableSet::drivable_area_at_step, py::arg("step"))
-            .def("reachable_set_at_step", &SemanticLabelingReachableSet::reachable_set_at_step, py::arg("step"))
-            .def("drivable_area", &SemanticLabelingReachableSet::drivable_area)
-            .def("reachable_set", &SemanticLabelingReachableSet::reachable_set)
-            .def("propagated_set", &SemanticLabelingReachableSet::propagated_set);
+                          &SemanticLabelingReachableSet::map_step_to_propositions_to_drivable_area);
+
+    py::class_<SemanticSplittingOTFReachableSet, shared_ptr<SemanticSplittingOTFReachableSet>, SemanticReachableSet>(m, "SemanticSplittingOTFReachableSet")
+            .def(py::init<SemanticConfigurationPtr const &, CollisionCheckerPtr const &,
+                         SemanticModelPtr const &, TrafficRuleInterfacePtr const &>(),
+                 py::arg("configuration"),
+                 py::arg("collision_checker"),
+                 py::arg("semantic_model"),
+                 py::arg("traffic_rule_interface"));
 }
 
 void export_reachable_set_labeler(py::module &m) {
