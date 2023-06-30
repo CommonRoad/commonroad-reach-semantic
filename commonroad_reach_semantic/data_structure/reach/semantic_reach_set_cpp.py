1de785157caa27c9abf727db88819d03abada015
Florian Lercher <florian.lercher@tum.de>
2023-06-30 13:54:02 +0200

    Extract dicts after pruning

--- commonroad_reach_semantic/data_structure/reach/semantic_reach_set_cpp.py
+++ commonroad_reach_semantic/data_structure/reach/semantic_reach_set_cpp.py
@@ -34,12 +34,12 @@ class CppSemanticReachableSet(SemanticReachableSet, ABC):
             self._reach.compute(step, step)
             self._list_steps_computed.append(step)
 
+        if self.config.reachable_set.prune_nodes_not_reaching_final_step:
+            self.prune_nodes_not_reaching_final_step()
+
         self.dict_step_to_drivable_area = self._reach.drivable_area()
         self.dict_step_to_reachable_set = self._reach.reachable_set()
         self.dict_step_to_propagated_set = self._reach.propagated_set()
-
-        if self.config.reachable_set.prune_nodes_not_reaching_final_step:
-            self.prune_nodes_not_reaching_final_step()
 
     def drivable_area_at_step(self, step: int):
         return self._reach.drivable_area_at_step(step)
