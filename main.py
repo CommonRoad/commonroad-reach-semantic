5db2325fdae257d470f166cad5a98eabc4ec479a
Florian Lercher <florian.lercher@tum.de>
2023-09-12 17:22:12 +0200

    Add yield scenario derived from ZAM intersection

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def main():
     # name_scenario = "USA_US101-6_1_T-1"
     # name_scenario = "USA_US101-15_2_T-1"
     # name_scenario = "ZAM_Intersection-1_1_T-1"
-    name_scenario = "ZAM_Intersection-1_2_T-1"
+    # name_scenario = "ZAM_Intersection-1_2_T-1"
     # name_scenario = "ZAM_Merge-1_1_T-1"
+    name_scenario = "ZAM_Yield-1_1_T-1"
     # name_scenario = "ESP_Monzon-2_2_T-1"
 
     # ==== build configuration
