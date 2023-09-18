4d521ba809b14543f1187a09ba428019d9fc5712
Florian Lercher <florian.lercher@tum.de>
2023-09-18 14:34:36 +0200

    Add title slide to video

--- reproduction/make_video.sh
+++ reproduction/make_video.sh
@@ -5,6 +5,7 @@ FPS=30
 y_offset=100
 fontsize=48
 
+video_dir="video"
 output_name="animation_yield.mp4"
 
 dirs=(
@@ -21,17 +22,20 @@ titles=(
   'Offline approach (after model checking)'
 )
 
-sections=""
+ffmpeg -framerate 0.2 -height 2160 -i "${video_dir}/title.svg" -c:v libx264 -r 30 -pix_fmt yuv420p "${video_dir}/title.mp4"
+sections="file '${PWD}/${video_dir}/title.mp4'"
+sections+=$'\n'
+
 for i in "${!dirs[@]}"; do
   dir=${dirs[$i]}
   title=${titles[$i]}
-  path="video/${dir}"
+  path="${video_dir}/${dir}"
   sections+="file '${PWD}/${path}.mp4'"
   sections+=$'\n'
 
   ffmpeg -framerate $svgs_per_second -pattern_type glob -height 2160 -i "${path}/*.svg" -c:v libx264 -r $FPS -pix_fmt yuv420p \
     -vf "drawtext=text='${title}':fontcolor=black:fontsize=${fontsize}:x=(w-text_w)/2+40:y=${y_offset}, drawtext=text='k = %{eif\:n\:d\:2}':fontcolor=black:fontsize=${fontsize}:x=w-tw-40:y=${y_offset}, pad=3840:2160:(ow-iw)/2:(oh-ih)/2" \
-    "video/$dir.mp4"
+    "${video_dir}/${dir}.mp4"
 done
 
 # concatenate videos
