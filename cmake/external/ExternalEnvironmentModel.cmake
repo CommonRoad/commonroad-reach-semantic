52face2a851489fa2021e83d16fe1197b222ecdf
Florian Lercher <florian.lercher@tum.de>
2023-09-21 14:45:45 +0200

    Add spdlog to CMake like in the environment model

--- cmake/external/ExternalEnvironmentModel.cmake
+++ cmake/external/ExternalEnvironmentModel.cmake
@@ -6,8 +6,6 @@ FetchContent_Declare(
         GIT_REPOSITORY git@gitlab.lrz.de:commonroad-traffic-rules/environment-model.git
         GIT_TAG "feature/obstacle-bounds-via-ccs"
 #        URL /home/lercher/tum/commonroad/environment-model
-
-#        FIND_PACKAGE_ARGS
 )
 
 FetchContent_MakeAvailable(EnvironmentModel)
