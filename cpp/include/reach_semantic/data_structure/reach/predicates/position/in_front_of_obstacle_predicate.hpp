c6c4a79a1d423b46c0416e53ae93b94387cde97d
Florian Lercher <florian.lercher@tum.de>
2023-09-22 16:55:02 +0200

    Improve documentation for in_front_of_obstacle_predicate

--- cpp/include/reach_semantic/data_structure/reach/predicates/position/in_front_of_obstacle_predicate.hpp
+++ cpp/include/reach_semantic/data_structure/reach/predicates/position/in_front_of_obstacle_predicate.hpp
@@ -6,13 +6,23 @@ using geometry::CurvilinearCoordinateSystem;
 
 namespace semantic_reach {
 /**
- * Evaluates whether the kth vehicle is in front of the pth vehicle.
+ * Predicate that is true if and only if the ego vehicle's rear is in front of the obstacle's front.
  */
 class InFrontOfObstaclePredicate : public CppPredicate {
   private:
     size_t obstacle_id;
     double ego_length;
 
+    /**
+     * Get the front of the obstacle at the given step.
+     *
+     * @param step The considered step.
+     * @param world Model of the environment.
+     * @param ego_ccs Curvilinear coordinate system of the ego vehicle.
+     * @return Front position inflated by half the length of the ego vehicle, or nullopt if the obstacle has no state at
+     * the given step or is not in the projection domain of the ego_ccs.
+     * @throws std::logic_error If the obstacle does not exist in the world.
+     */
     [[nodiscard]] std::optional<double>
     _get_obstacle_front(int step, const std::shared_ptr<World> &world,
                         const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const;
@@ -28,6 +38,10 @@ class InFrontOfObstaclePredicate : public CppPredicate {
   public:
     /**
      * Constructor for in front of obstacle predicate.
+     *
+     * @param negated Whether the predicate is negated.
+     * @param obstacle_id ID of the obstacle.
+     * @param ego_length Length of the ego vehicle.
      */
     InFrontOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_length);
 };
