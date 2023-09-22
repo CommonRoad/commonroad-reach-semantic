f0b7cc73abd4152ad46215826fcd8a2907617ef9
Florian Lercher <florian.lercher@tum.de>
2023-09-22 10:21:33 +0200

    Add hack for compiling pybind with env model

--- python_binding/pybind.cpp
+++ python_binding/pybind.cpp
@@ -1,9 +1,14 @@
 #include "pybind.hpp"
 
+#include <commonroad_cpp/predicates/predicate_config.h>
+
 PYBIND11_MODULE(pycrreachs, m) {
     m.doc() = "Pybind module for semantic reachable set.";
 
     export_data_structures(m);
     export_utility(m);
     export_reach(m);
+
+    // TODO: remove this dirty fix for undefined symbol checkParameterValidity once it is fixed in env model
+    PredicateParameters p{};
 }
