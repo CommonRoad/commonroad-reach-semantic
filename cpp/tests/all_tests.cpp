c6a60e7f3140043561dfbc6d27170e5ab14ae689
Florian Lercher <florian.lercher@tum.de>
2023-09-22 16:53:35 +0200

    Add hack for checkParameterValidity problem in tests

--- cpp/tests/all_tests.cpp
+++ cpp/tests/all_tests.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <commonroad_cpp/predicates/predicate_config.h>
+
 int main(int argc, char **argv) {
+    // TODO: remove this dirty fix for undefined symbol checkParameterValidity once it is fixed in env model
+    PredicateParameters p{};
+
     testing::InitGoogleTest(&argc, argv);
     // set the gtest death test style to threadsafe
     testing::FLAGS_gtest_death_test_style = "threadsafe";
