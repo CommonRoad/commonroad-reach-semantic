53e0b541bc95f69c63128e5130e6f8d639f1a4bb
Florian Lercher <florian.lercher@tum.de>
2023-10-18 11:20:28 +0200

    Switch main to merge scenario and clean up config

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def main():
     # ==== specify scenario
     # name_scenario = "ZAM_Intersection-1_1_T-1"
     # name_scenario = "ZAM_Intersection-1_2_T-1"
-    # name_scenario = "ZAM_Merge-1_1_T-1"
-    name_scenario = "ZAM_Yield-1_1_T-1"
+    name_scenario = "ZAM_Merge-1_1_T-1"
+    # name_scenario = "ZAM_Yield-1_1_T-1"
     # name_scenario = "ESP_Monzon-2_2_T-1"
 
     # ==== build configuration
