4a616db4f29cb4b91f7a21b684e13608468047fa
Florian Lercher <florian.lercher@tum.de>
2023-10-24 10:45:02 +0200

    Update Dockerfile to use rapid-prototyping-setup ci-lite as base image

--- ci/deps/build_docker.sh
+++ ci/deps/build_docker.sh
@@ -1,6 +1,8 @@
+TAG="ci-cpp-env-model"
 docker login gitlab.lrz.de:5005
+git clone -b development --recurse-submodules --single-branch --depth 1 git@gitlab.lrz.de:cps/commonroad-drivability-checker.git dc
 git clone -b develop --recurse-submodules --single-branch --depth 1 git@gitlab.lrz.de:cps/commonroad-reachable-set.git reach
-docker build --build-arg LOCAL_CRREACH_DIR="./reach" -t gitlab.lrz.de:5005/cps/commonroad-reach-semantic/deps:ci .
-rm -rf reach
-docker push gitlab.lrz.de:5005/cps/commonroad/commonroad-reach-semantic/deps:ci
+docker build --build-arg LOCAL_CRDC_DIR="./dc" --build-arg LOCAL_CRREACH_DIR="./reach" -t gitlab.lrz.de:5005/cps/commonroad/commonroad-reach-semantic/deps:$TAG .
+rm -rf reach dc
+docker push gitlab.lrz.de:5005/cps/commonroad/commonroad-reach-semantic/deps:$TAG
 docker logout gitlab.lrz.de:5005
