09bb6ab17bbb5f4bf1821d37564293b9b27bc90e
Florian Lercher <florian.lercher@tum.de>
2023-10-24 10:53:37 +0200

    Update branch of env model

--- cmake/external/ExternalEnvironmentModel.cmake
+++ cmake/external/ExternalEnvironmentModel.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
         EnvironmentModel
         GIT_REPOSITORY git@gitlab.lrz.de:commonroad-traffic-rules/environment-model.git
-        GIT_TAG "feature/obstacle-bounds-via-ccs"
+        GIT_TAG "develop"
 #        URL /home/lercher/tum/commonroad/environment-model
 )
 
