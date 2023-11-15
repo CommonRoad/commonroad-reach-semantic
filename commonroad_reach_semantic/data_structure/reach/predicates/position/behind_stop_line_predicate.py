50fbcdb7f3e48cae08160977cd73ac4300d3846c
ge36nij <yuanfei.lin@tum.de>
2023-11-15 18:30:18 +0100

    wip: include the feedback for stopline calc

--- commonroad_reach_semantic/data_structure/reach/predicates/position/behind_stop_line_predicate.py
+++ commonroad_reach_semantic/data_structure/reach/predicates/position/behind_stop_line_predicate.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Tuple
 
 from commonroad_reach import pycrreach
 from commonroad_reach.data_structure.reach.reach_node import ReachNode
@@ -20,6 +20,7 @@ class BehindStopLinePredicate(predicate.Predicate):
     def to_proposition(self) -> str:
         return Prop.behind_stop_line()
 
+    @predicate.needs_lanelets_set
     def _restrict_reach_node_mandatory(
         self,
         step: int,
@@ -35,37 +36,25 @@ class BehindStopLinePredicate(predicate.Predicate):
             return []
 
         # Find lanelet IDs that are both in stop_line_lanelet_ids and node_lanelet_ids
-        occupied_lanelet_ids = set(stop_line_lanelet_ids) & set(node_lanelet_ids or [])
+        occupied_lanelet_ids = stop_line_lanelet_ids & node_lanelet_ids
 
         # Return empty list if no occupied lanelets with stop lines are found
         if not occupied_lanelet_ids:
             return []
 
+        result_nodes = []
         for lanelet_id in occupied_lanelet_ids:
-            # Obtain the longitudinal coordinate of the stop line
-            stop_line_s = self._get_stop_line(semantic_model, lanelet_id)
-
-            if stop_line_s is not None:
-                # Calculate positions relative to the stop line
-                dis_stop_line = semantic_model.config.traffic_rule.dis_stop_line
-
-                # Additional consideration of the vehicle length
-                vehicle_length_add = semantic_model.config.vehicle.ego.length / 2
-                if semantic_model.config.planning.reference_point == "REAR":
-                    vehicle_length_add += semantic_model.config.vehicle.ego.wb_rear_axle
-
-                # stop_line_s - vehicle_front < dis_stop_line
-                min_position = stop_line_s - dis_stop_line - vehicle_length_add
-                # vehicle_front < stop_line_s
-                max_position = stop_line_s - vehicle_length_add
-
+            safe_stop_positions = self._get_safe_stopping_positions(semantic_model, lanelet_id)
+            if safe_stop_positions is not None:
                 # Adjust the reach node positions
-                reach_node.intersect_in_position_domain(
-                    p_lon_min=min_position, p_lon_max=max_position
+                new_node = reach_node.clone()
+                new_node.intersect_in_position_domain(
+                    p_lon_min=safe_stop_positions[0], p_lon_max=safe_stop_positions[1]
                 )
+                result_nodes.append(new_node) if new_node else None
+        return result_nodes
 
-        return [reach_node] if reach_node else []
-
+    @predicate.needs_lanelets_set
     def _restrict_reach_node_forbidden(
         self,
         step: int,
@@ -81,7 +70,7 @@ class BehindStopLinePredicate(predicate.Predicate):
             return [reach_node]
 
         # Find lanelet IDs that are both in stop_line_lanelet_ids and node_lanelet_ids
-        occupied_lanelet_ids = set(stop_line_lanelet_ids) & set(node_lanelet_ids or [])
+        occupied_lanelet_ids = stop_line_lanelet_ids & node_lanelet_ids
 
         # Return early if no occupied lanelets with stop lines are found
         if not occupied_lanelet_ids:
@@ -92,33 +81,19 @@ class BehindStopLinePredicate(predicate.Predicate):
             new_nodes = []  # Temporarily store new nodes for this iteration
             for lanelet_id in occupied_lanelet_ids:
                 # Obtain the longitudinal coordinate of the stop line
-                stop_line_s = self._get_stop_line(semantic_model, lanelet_id)
+                safe_stop_positions = self._get_safe_stopping_positions(semantic_model, lanelet_id)
 
-                if stop_line_s is not None:
-                    # Calculate positions relative to the stop line
-                    dis_stop_line = semantic_model.config.traffic_rule.dis_stop_line
-
-                    # Additional consideration of the vehicle length
-                    vehicle_length_add = semantic_model.config.vehicle.ego.length / 2
-                    if semantic_model.config.planning.reference_point == pycrreach.ReferencePoint.REAR:
-                        vehicle_length_add += semantic_model.config.vehicle.ego.wb_rear_axle
-
-                    # behind the stop line where: stop_line_s - vehicle_front > dis_stop_line
-                    behind_stop_line_position = (
-                        stop_line_s - dis_stop_line - vehicle_length_add
-                    )
-                    # in front of stop line where: stop_line_s < vehicle_front
-                    in_front_stop_line_position = stop_line_s - vehicle_length_add
+                if safe_stop_positions is not None:
 
                     # Clone and adjust the reach node positions
                     behind_sl_node = node.clone()
                     behind_sl_node.intersect_in_position_domain(
-                        p_lon_max=behind_stop_line_position
+                        p_lon_max=safe_stop_positions[0]
                     )
 
                     in_front_sl_node = node.clone()
                     in_front_sl_node.intersect_in_position_domain(
-                        p_lon_min=in_front_stop_line_position
+                        p_lon_min=safe_stop_positions[1]
                     )
 
                     new_nodes.extend([behind_sl_node, in_front_sl_node])
@@ -127,10 +102,10 @@ class BehindStopLinePredicate(predicate.Predicate):
         return result_nodes if result_nodes else [reach_node]
 
     @staticmethod
-    def _get_stop_line(
+    def _get_safe_stopping_positions(
         semantic_model: SemanticModel, lanelet_with_sl_id: int
-    ) -> Optional[float]:
-        """Gets the longitudinal coordinate of the stop line."""
+    ) -> Optional[Tuple[float, float]]:
+        """Gets the safe minimum and maximum stopping positions relative to the stop line."""
 
         lanelet = semantic_model.config.scenario.lanelet_network.find_lanelet_by_id(
             lanelet_with_sl_id
@@ -144,4 +119,21 @@ class BehindStopLinePredicate(predicate.Predicate):
         start_s = convert_coords(*stop_line.start)[0]
         end_s = convert_coords(*stop_line.end)[0]
 
-        return min(start_s, end_s) if start_s and end_s else None
+        stop_line_s = min(start_s, end_s) if start_s and end_s else None
+
+        if stop_line_s is not None:
+            # Calculate positions relative to the stop line
+            dis_stop_line = semantic_model.config.traffic_rule.dis_stop_line
+
+            # Additional consideration of the vehicle length
+            vehicle_length_add = semantic_model.config.vehicle.ego.length / 2
+            if semantic_model.config.planning.reference_point == "REAR":
+                vehicle_length_add += semantic_model.config.vehicle.ego.wb_rear_axle
+
+            # stop_line_s - vehicle_front < dis_stop_line
+            min_position = stop_line_s - dis_stop_line - vehicle_length_add
+            # vehicle_front < stop_line_s
+            max_position = stop_line_s - vehicle_length_add
+
+            return min_position, max_position
+        return None
