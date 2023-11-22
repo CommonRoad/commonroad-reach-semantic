1cf68796a36acc7a4313213b822dc1c1e356805e
Florian Lercher <florian.lercher@tum.de>
2023-11-22 15:42:02 +0100

    Update Dockerfile

--- ci/deps/build_docker.sh
+++
@@ -1,6 +0,0 @@
-docker login gitlab.lrz.de:5005
-git clone -b develop --recurse-submodules --single-branch --depth 1 git@gitlab.lrz.de:cps/commonroad-reachable-set.git reach
-docker build --build-arg LOCAL_CRREACH_DIR="./reach" -t gitlab.lrz.de:5005/cps/commonroad-reach-semantic/deps:ci .
-rm -rf reach
-docker push gitlab.lrz.de:5005/cps/commonroad/commonroad-reach-semantic/deps:ci
-docker logout gitlab.lrz.de:5005
--- ci/deps/updating_docker_image.md
+++
@@ -1,9 +0,0 @@
-## Updating the Docker Image for CI
-
-When CommonRoad-Reach changes or if we need any new dependencies, we need to update the Docker image that is used for CI.
-
-To update the Docker image, simply run
-```bash
-bash update_docker_image.sh
-```
-and enter your LRZ GitLab credentials when prompted.
