c269628a9ea5d900bceeb700cd12b083a1ad5eb9
Klaudia Tarabasz <klaudia.tarabasz@tum.de>
2023-11-23 13:43:26 +0100

    predicates behind, left and right of obstacle

---
+++ cpp/src/data_structure/reach/predicates/position/right_of_obstacle_predicate.cpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "reach_semantic/data_structure/reach/predicates/cpp_predicate.hpp"
+
+using geometry::CurvilinearCoordinateSystem;
+
+namespace semantic_reach {
+
+class RightOfObstaclePredicate : public CppPredicate {
+  private:
+    size_t obstacle_id;
+    double ego_length;
+
+    [[nodiscard]] std::optional<double>
+    _get_obstacle_right(int step, const std::shared_ptr<World> &world,
+                        const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const;
+
+    [[nodiscard]] std::vector<reach::ReachNodePtr> _restrict_reach_node_mandatory(
+        int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
+        const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const override;
+
+    [[nodiscard]] std::vector<reach::ReachNodePtr> _restrict_reach_node_forbidden(
+        int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
+        const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const override;
+
+  public:
+
+    RightOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_length);
+};
+} // namespace semantic_reach
