d08a19824a1e021485999667a220b7ffacc7643f
Klaudia Tarabasz <klaudia.tarabasz@tum.de>
2023-12-11 15:56:28 +0100

    safe distance - calculation of safe distance and part of restrict node

---
+++ cpp/include/reach_semantic/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "reach_semantic/data_structure/reach/predicates/cpp_predicate.hpp"
+
+using geometry::CurvilinearCoordinateSystem;
+
+namespace semantic_reach {
+
+class KeepSafeDistancePrecPredicate : CppPredicate {
+    private:
+        size_t obstacle_id;
+        double ego_length;
+
+        [[nodiscard]] std::vector<reach::ReachNodePtr> _restrict_reach_node_mandatory(
+                int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
+                const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const override;
+
+        [[nodiscard]] std::vector<reach::ReachNodePtr> _restrict_reach_node_forbidden(
+                int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
+                const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const override;
+
+
+        [[nodiscard]] std::pair<double, double> _determine_safe_position(
+            int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
+            const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs, double ego_speed) const;
+
+    public:
+        KeepSafeDistancePrecPredicate(bool negated, size_t obstacle_id, double ego_length);
+
+};
+}
+
+
---
+++ cpp/src/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tarabasz on 12/1/23.
+//
+
+#include "keeps_safe_distance_prec_predicate.h"
