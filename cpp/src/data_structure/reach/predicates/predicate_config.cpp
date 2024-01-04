f157d0a9cbdcc1bc5e8e890a4609086b89f0b965
Florian Lercher <florian.lercher@tum.de>
2024-01-04 16:20:15 +0100

    Adjust predicate config to changes in environment model

--- cpp/src/data_structure/reach/predicates/predicate_config.cpp
+++ cpp/src/data_structure/reach/predicates/predicate_config.cpp
@@ -5,9 +5,9 @@ using namespace semantic_reach;
 PredicateConfiguration::PredicateConfiguration(const SemanticConfiguration &config) {
     traffic_rule_params = PredicateParameters{};
     auto traffic_rule_config = config.config_traffic_rule;
-    traffic_rule_params.standstillError = traffic_rule_config.backward_driving_v_err;
-    traffic_rule_params.aBrakingIntersection = traffic_rule_config.acceleration_braking_hard;
-    traffic_rule_params.dBrakingIntersection = traffic_rule_config.distance_braking;
+    traffic_rule_params.updateParam("standstillError", traffic_rule_config.backward_driving_v_err);
+    traffic_rule_params.updateParam("aBrakingIntersection", traffic_rule_config.acceleration_braking_hard);
+    traffic_rule_params.updateParam("dBrakingIntersection", traffic_rule_config.distance_braking);
     traffic_rule_params.checkParameterValidity();
 
     ego_length = config.config_vehicle.ego.length;
