fc35df71c43a57febfa0d792c810ee8d4e06ca67
Florian Lercher <florian.lercher@tum.de>
2024-02-22 16:11:15 +0100

    Refactor documentation for safe distance predicate

--- cpp/include/reach_semantic/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.hpp
+++ cpp/include/reach_semantic/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.hpp
@@ -14,7 +14,7 @@ class KeepSafeDistancePrecPredicate : public CppPredicate {
     double ego_length;
     double ego_reaction_time;
     double ego_deceleration;
-    double vehicle_deceleration; // deceleration of other vehicle
+    double other_deceleration; // deceleration of other vehicle specified by obstacle_id
 
     [[nodiscard]] std::vector<reach::ReachNodePtr> _restrict_reach_node_mandatory(
         int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
@@ -24,17 +24,51 @@ class KeepSafeDistancePrecPredicate : public CppPredicate {
         int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
         const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs) const override;
 
+    /**
+     * Evaluate the safe position function at the given velocity.
+     *
+     * @param step Current time step
+     * @param world Environment model
+     * @param ego_ccs Curvilinear coordinate system of the ego vehicle
+     * @param ego_velocity Velocity at which to evaluate the safe position function
+     * @return Maximal longitudinal position of the ego vehicle is still safe at the given velocity or nullopt if the
+     * safe position cannot be computed due to prediction or projection domain issues.
+     */
     [[nodiscard]] std::optional<double>
     _determine_safe_position(int step, const std::shared_ptr<World> &world,
                              const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs,
-                             double ego_speed) const;
+                             double ego_velocity) const;
 
-    [[nodiscard]] double _determine_slope(double ego_speed) const;
+    /**
+     * Determine the slope (derivative) of the safe position function at the given velocity.
+     *
+     * @param ego_velocity Velocity at which to determine the slope
+     * @return Slope of the safe position function at ego_velocity
+     */
+    [[nodiscard]] double _determine_slope(double ego_velocity) const;
 
+    /**
+     * Compute the coefficients of the halfspace that defines the safe region.
+     *
+     * @param ego_velocity_support Support point for velocity
+     * @param slope Slope of the safe position function at the support point
+     * @param safe_pos Value of the safe position function at the support point
+     * @return Halfspace coefficients (a, b, c) such that a * s + b * v <= c defines the safe region
+     */
     [[nodiscard]] static std::tuple<double, double, double>
-    _compute_parameters_of_halfspace(double ego_velocity_support, double slope, double safe_pos);
+    _compute_halfspace_coefficients(double ego_velocity_support, double slope, double safe_pos);
 
   public:
+    /**
+     * Constructor for safe distance predicate.
+     *
+     * @param negated Whether the predicate is negated.
+     * @param obstacle_id ID of the other vehicle to keep a safe distance to.
+     * @param ego_length Length of the ego vehicle.
+     * @param ego_reaction_time Reaction time of the ego vehicle.
+     * @param ego_deceleration Deceleration of the ego vehicle.
+     * @param vehicle_deceleration Deceleration of the other vehicle.
+     */
     KeepSafeDistancePrecPredicate(bool negated, size_t obstacle_id, double ego_length, double ego_reaction_time,
                                   double ego_deceleration, double vehicle_deceleration);
 };
--- cpp/src/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.cpp
+++ cpp/src/data_structure/reach/predicates/position/keeps_safe_distance_prec_predicate.cpp
@@ -10,7 +10,7 @@ KeepSafeDistancePrecPredicate ::KeepSafeDistancePrecPredicate(bool negated, size
                                                               double vehicle_deceleration)
     : CppPredicate(negated, false), obstacle_id(obstacle_id), ego_length(ego_length),
       ego_reaction_time(ego_reaction_time), ego_deceleration(ego_deceleration),
-      vehicle_deceleration(vehicle_deceleration) {}
+      other_deceleration(vehicle_deceleration) {}
 
 std::vector<reach::ReachNodePtr> KeepSafeDistancePrecPredicate::_restrict_reach_node_mandatory(
     int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
@@ -31,7 +31,7 @@ std::vector<reach::ReachNodePtr> KeepSafeDistancePrecPredicate::_restrict_reach_
 
         auto slope = _determine_slope(ego_velocity_support);
 
-        auto [a, b, c] = _compute_parameters_of_halfspace(ego_velocity_support, slope, safe_position_opt.value());
+        auto [a, b, c] = _compute_halfspace_coefficients(ego_velocity_support, slope, safe_position_opt.value());
         reach_node->polygon_lon->intersect_halfspace(a, b, c);
     }
     return {reach_node};
@@ -58,7 +58,7 @@ std::vector<reach::ReachNodePtr> KeepSafeDistancePrecPredicate::_restrict_reach_
 
         auto slope = _determine_slope(ego_velocity_support);
 
-        auto [a, b, c] = _compute_parameters_of_halfspace(ego_velocity_support, slope, safe_position_opt.value());
+        auto [a, b, c] = _compute_halfspace_coefficients(ego_velocity_support, slope, safe_position_opt.value());
         auto node = reach_node->clone();
         // negate the halfspace parameters to get the forbidden region
         node->polygon_lon->intersect_halfspace(-a, -b, -c);
@@ -67,10 +67,9 @@ std::vector<reach::ReachNodePtr> KeepSafeDistancePrecPredicate::_restrict_reach_
     return v;
 }
 
-// It returns max lon position, that is still safe for ego
 std::optional<double> KeepSafeDistancePrecPredicate::_determine_safe_position(
     int step, const std::shared_ptr<World> &world,
-    const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs, double ego_speed) const {
+    const std::shared_ptr<geometry::CurvilinearCoordinateSystem> &ego_ccs, double ego_velocity) const {
 
     auto obstacle = world->findObstacle(obstacle_id);
     std::shared_ptr<State> obstacle_state;
@@ -82,24 +81,24 @@ std::optional<double> KeepSafeDistancePrecPredicate::_determine_safe_position(
 
     double vehicle_speed = obstacle_state->getVelocity(); // speed of other vehicle
 
-    double safe_dist = (vehicle_speed * vehicle_speed) / (-2 * abs(vehicle_deceleration)) -
-                       (ego_speed * ego_speed) / (-2 * abs(ego_deceleration)) + ego_speed * ego_reaction_time;
+    double safe_dist = (vehicle_speed * vehicle_speed) / (-2 * abs(other_deceleration)) -
+                       (ego_velocity * ego_velocity) / (-2 * abs(ego_deceleration)) + ego_velocity * ego_reaction_time;
 
     return obstacle->rearS(step, ego_ccs) - safe_dist - ego_length / 2.0;
 }
 
-double KeepSafeDistancePrecPredicate::_determine_slope(double ego_speed) const {
-    double v_prim = ego_speed / abs(ego_deceleration) + ego_reaction_time;
+double KeepSafeDistancePrecPredicate::_determine_slope(double ego_velocity) const {
+    double v_prime = ego_velocity / abs(ego_deceleration) + ego_reaction_time;
     // negate the slope of the safe distance function to get the slope of the safe position function (safe distance is
     // negated there)
-    return -v_prim;
+    return -v_prime;
 }
 
 std::tuple<double, double, double>
-KeepSafeDistancePrecPredicate::_compute_parameters_of_halfspace(double ego_velocity_support, double slope,
-                                                                double safe_pos) {
-    // x <= slope * y + safe_pos - slope * ego_velocity_support;
-    // in the form ax + by <= c:
-    // 1 * x + (-slope) * y <= safe_pos - slope * ego_velocity_support
+KeepSafeDistancePrecPredicate::_compute_halfspace_coefficients(double ego_velocity_support, double slope,
+                                                               double safe_pos) {
+    // position <= slope * velocity + safe_pos - slope * ego_velocity_support;
+    // in the form a * position + b * velocity <= c:
+    // 1 * position + (-slope) * velocity <= safe_pos - slope * ego_velocity_support
     return {1, -slope, safe_pos - slope * ego_velocity_support};
 }
