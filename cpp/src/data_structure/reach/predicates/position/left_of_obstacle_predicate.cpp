14a2c8ceb285ca8f5ffcd0546091749e2318e283
Florian Lercher <florian.lercher@tum.de>
2024-02-23 17:58:14 +0100

    Fix clang tidy warnings

--- cpp/src/data_structure/reach/predicates/position/left_of_obstacle_predicate.cpp
+++ cpp/src/data_structure/reach/predicates/position/left_of_obstacle_predicate.cpp
@@ -6,8 +6,8 @@
 using namespace semantic_reach;
 using geometry::CurvilinearCoordinateSystem;
 
-LeftOfObstaclePredicate::LeftOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_length)
-    : CppPredicate(negated, false), obstacle_id(obstacle_id), ego_width(ego_length) {}
+LeftOfObstaclePredicate::LeftOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_width)
+    : CppPredicate(negated, false), obstacle_id(obstacle_id), ego_width(ego_width) {}
 
 std::vector<reach::ReachNodePtr> LeftOfObstaclePredicate::_restrict_reach_node_mandatory(
     int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
--- cpp/src/data_structure/reach/predicates/position/right_of_obstacle_predicate.cpp
+++ cpp/src/data_structure/reach/predicates/position/right_of_obstacle_predicate.cpp
@@ -6,8 +6,8 @@
 using namespace semantic_reach;
 using geometry::CurvilinearCoordinateSystem;
 
-RightOfObstaclePredicate::RightOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_length)
-    : CppPredicate(negated, false), obstacle_id(obstacle_id), ego_width(ego_length) {}
+RightOfObstaclePredicate::RightOfObstaclePredicate(bool negated, size_t obstacle_id, double ego_width)
+    : CppPredicate(negated, false), obstacle_id(obstacle_id), ego_width(ego_width) {}
 
 std::vector<reach::ReachNodePtr> RightOfObstaclePredicate::_restrict_reach_node_mandatory(
     int step, const reach::ReachNodePtr &reach_node, const std::shared_ptr<World> &world,
