98fa05b7e080db9323661e9d84b42262d69e288e
Florian Lercher <florian.lercher@tum.de>
2024-03-13 19:29:14 +0100

    Update Docker image

--- ci/docker/build_docker.sh
+++ ci/docker/build_docker.sh
@@ -4,7 +4,7 @@ REGISTRY=gitlab.lrz.de:5005
 IMAGE=$REGISTRY/cps/commonroad/commonroad-reach-semantic/ci
 
 # Update this when creating a new version of the image
-TAG=1.0
+TAG=2.0
 
 docker login $REGISTRY
 docker build -t $IMAGE:$TAG .
