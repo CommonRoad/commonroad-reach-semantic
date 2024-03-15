de49d26d7af148174ec68bb1c5bc984a8c87a3bf
Florian Lercher <florian.lercher@tum.de>
2024-03-15 10:22:17 +0100

    Use public git repo of commonroad-reach

--- cmake/external/ExternalReach.cmake
+++ cmake/external/ExternalReach.cmake
@@ -2,9 +2,10 @@ include(FetchContent)
 
 FetchContent_Declare(
         crreach
-        GIT_REPOSITORY git@gitlab.lrz.de:cps/commonroad-reachable-set.git
-        GIT_TAG sync-dependencies-with-env-model
-        #    URL /home/lercher/tum/commonroad-reachable-set
+        GIT_REPOSITORY https://github.com/CommonRoad/commonroad-reachable-set.git
+        GIT_TAG 3b59d54f591e578418063ede614adad020158311
+        # GIT_TAG develop
+        # URL /home/lercher/tum/commonroad-reachable-set
 )
 
 FetchContent_MakeAvailable(crreach)
