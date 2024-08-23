82819bc9f2aff82fa044b4f0fea03edd11b7838f
Xuyuan Han <xuyuan.han@outlook.com>
2024-08-23 20:14:23 +0800

    build(ExternalEnvironmentModel.cmake): add PAT to clone the repo environment-model
    Signed-off-by: Xuyuan Han <xuyuan.han@outlook.com>

--- cmake/external/ExternalEnvironmentModel.cmake
+++ cmake/external/ExternalEnvironmentModel.cmake
@@ -5,7 +5,8 @@ set(EXTERNAL_CRDC_FORCE ON)
 
 FetchContent_Declare(
         EnvironmentModel
-        GIT_REPOSITORY git@gitlab.lrz.de:commonroad-traffic-rules/environment-model.git
+        # GIT_REPOSITORY git@gitlab.lrz.de:commonroad-traffic-rules/environment-model.git
+        GIT_REPOSITORY https://autoware_docker:glpat-Fy7MZR5qzNPTrY_HdKEM@gitlab.lrz.de/cps/commonroad/environment-model.git
         GIT_TAG 3030c8501ed9d7c48925ecb642d496a60bd431ec
         #    URL /home/lercher/tum/commonroad/environment-model
 )
