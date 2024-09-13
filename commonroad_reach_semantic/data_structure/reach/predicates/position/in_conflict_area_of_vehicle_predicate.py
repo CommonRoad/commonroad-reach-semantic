70ab6ad6713ca7be81e391e8c82071ae5bf065ad
Yuanfei <yuanfei.lin@tum.de>
2024-09-13 15:47:45 +0200

    fix in conflict area for CARLA scenario

--- commonroad_reach_semantic/data_structure/reach/predicates/position/in_conflict_area_of_vehicle_predicate.py
+++ commonroad_reach_semantic/data_structure/reach/predicates/position/in_conflict_area_of_vehicle_predicate.py
@@ -38,34 +38,36 @@ class InConflictAreaOfVehiclePredicate(predicate.Predicate):
     def _restrict_reach_node_forbidden(self, step: int, reach_node: ReachNode, semantic_model: SemanticModel,
                                        node_lanelet_ids: Set[int]) -> List[ReachNode]:
         # Retrieve the vehicle object using its ID
-        # vehicle = semantic_model.vehicle_model.find_vehicle_by_id(self.vehicle_id)
+        vehicle = semantic_model.vehicle_model.find_vehicle_by_id(self.vehicle_id)
 
         # Get the lanelet IDs for the ego vehicle and the other vehicle
-        # #lanelets_dir_ego = set(semantic_model.config.planning.route.lanelet_ids)
-        # lanelets_dir_other = set(vehicle.lanelets_dir)
+        lanelets_dir_ego = set(semantic_model.config.planning.route.lanelet_ids)
+        lanelets_dir_other = set(vehicle.lanelets_dir)
 
         # Determine the lanelets that are in the other vehicle's path but not in the ego vehicle's path
-        # disjoint_other = list(lanelets_dir_other - lanelets_dir_ego)
-
-        lanelet = semantic_model.config.scenario.lanelet_network.find_lanelet_by_id(
-                    11
+        disjoint_other = list(lanelets_dir_other - lanelets_dir_ego)
+        if not disjoint_other:
+            return [reach_node]
+        for lanelet_id in disjoint_other:
+            lanelet = semantic_model.config.scenario.lanelet_network.find_lanelet_by_id(
+                        lanelet_id
+                    )
+            convert_coords = (
+                    semantic_model.config.planning.CLCS.convert_to_curvilinear_coords
                 )
-        convert_coords = (
-                semantic_model.config.planning.CLCS.convert_to_curvilinear_coords
-            )
 
-        start_s, end_s = [
-            convert_coords(*coord)[0] for coord in [lanelet.right_vertices[int(len(lanelet.right_vertices)/2)],
-                                                    lanelet.right_vertices[int(len(lanelet.right_vertices)/2)-1]]
-        ]
-        conflict_s = min(start_s, end_s)
+            start_s, end_s = [
+                convert_coords(*coord)[0] for coord in [(lanelet.right_vertices[0] + lanelet.right_vertices[-1])/2,
+                                                        (lanelet.left_vertices[0] + lanelet.left_vertices[-1])/2]
+            ]
+            conflict_s = min(start_s, end_s)
 
-        # Additional consideration of the vehicle length
-        vehicle_length_add = semantic_model.config.vehicle.ego.length / 2
-        if semantic_model.config.planning.reference_point == "REAR":
-            vehicle_length_add += semantic_model.config.vehicle.ego.wb_rear_axle
+            # Additional consideration of the vehicle length
+            vehicle_length_add = semantic_model.config.vehicle.ego.length / 3 - semantic_model.config.vehicle.ego.wheelbase/ 2
+            # if semantic_model.config.planning.reference_point == "REAR":
+            #     vehicle_length_add += semantic_model.config.vehicle.ego.wb_rear_axle
 
-        reach_node.intersect_in_position_domain(p_lon_max=conflict_s - vehicle_length_add)
+            reach_node.intersect_in_position_domain(p_lon_max=conflict_s - vehicle_length_add)
         return [reach_node]
 
 
